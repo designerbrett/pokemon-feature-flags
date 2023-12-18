4f1f4d574bbf63d5bfc477cbff52ad35b6f16d42
designerbrett <brettdesigner@gmail.com>
2023-12-18 15:55:05 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
-import React from "react";
-import Pokemon from "./Pokemon";
-import { withLDProvider } from "launchdarkly-react-client-sdk";
+import React, { useState } from 'react';
 
 function App() {
+  const [currentTotal, setCurrentTotal] = useState('');
+  const [yearsToRetire, setYearsToRetire] = useState('');
+  const [desiredAmount, setDesiredAmount] = useState('');
+  const [result, setResult] = useState(null);
+
+  const calculateRetirement = () => {
+    const total = parseFloat(currentTotal);
+    const years = parseFloat(yearsToRetire);
+    const goal = parseFloat(desiredAmount);
+
+    if (!isNaN(total) && !isNaN(years) && !isNaN(goal) && years > 0) {
+      const monthlyContribution = (goal - total) / (years * 12);
+      setResult(`To reach your goal, contribute approximately $${monthlyContribution.toFixed(2)} per month.`);
+    } else {
+      setResult('Please enter valid numbers for all fields.');
+    }
+  };
+
   return (
-    <>
-      <Pokemon />
-    </>
+    <div>
+      <h1>Retirement Planner</h1>
+      <div>
+        <label>
+          Current Retirement Total:
+          <input type="number" value={currentTotal} onChange={(e) => setCurrentTotal(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Years Till Retirement:
+          <input type="number" value={yearsToRetire} onChange={(e) => setYearsToRetire(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Desired Retirement Amount:
+          <input type="number" value={desiredAmount} onChange={(e) => setDesiredAmount(e.target.value)} />
+        </label>
+      </div>
+      <button onClick={calculateRetirement}>Calculate</button>
+      {result && <p>{result}</p>}
+    </div>
   );
 }
 
-export default withLDProvider({
-  clientSideID: import.meta.env
-    .VITE_LD_CLIENT_KEY,
-  user: {
-    key: "user_key",
-    name: "User name",
-    email: "User@email.com",
-  },
-})(App);
+export default App;
