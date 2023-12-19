a7532e2bb5a7a993ac370fa9974bfa9509d6ae12
designerbrett <brettdesigner@gmail.com>
2023-12-19 11:36:43 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const RetirementPlanner = () => {
   const [currentAssets, setCurrentAssets] = useState(localStorage.getItem('currentAssets') || '');
   const [yearsTillRetirement, setYearsTillRetirement] = useState(localStorage.getItem('yearsTillRetirement') || '');
   const [estimatedReturn, setEstimatedReturn] = useState(localStorage.getItem('estimatedReturn') || '');
+  const [contributionAmount, setContributionAmount] = useState(localStorage.getItem('contributionAmount') || '');
   const [compoundingFrequency, setCompoundingFrequency] = useState('yearly');
   const [results, setResults] = useState([]);
 
@@ -19,26 +20,29 @@ const RetirementPlanner = () => {
     localStorage.setItem('currentAssets', currentAssets);
     localStorage.setItem('yearsTillRetirement', yearsTillRetirement);
     localStorage.setItem('estimatedReturn', estimatedReturn);
+    localStorage.setItem('contributionAmount', contributionAmount);
     localStorage.setItem('compoundingFrequency', compoundingFrequency);
 
     calculateRetirementPlan();
-  }, [currentAssets, yearsTillRetirement, estimatedReturn, compoundingFrequency]);
+  }, [currentAssets, yearsTillRetirement, estimatedReturn, contributionAmount, compoundingFrequency]);
 
   const calculateRetirementPlan = () => {
     let currentTotal = parseFormattedNumber(currentAssets);
     const returnRate = parseFloat(estimatedReturn) / 100;
-    const compoundingFactor = compoundingFrequency === 'yearly' ? 1 : 12; // Adjust the compounding factor based on frequency
+    const compoundingFactor = compoundingFrequency === 'yearly' ? 1 : 12;
+    const contribution = parseFormattedNumber(contributionAmount);
 
     const newResults = Array.from({ length: parseInt(yearsTillRetirement) * compoundingFactor }, (_, index) => {
       const yearlyReturn = currentTotal * returnRate;
       const monthlyReturn = yearlyReturn / 12;
 
-      currentTotal = currentTotal + (compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn);
+      currentTotal = currentTotal + (compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn) + contribution;
 
       return {
         period: index + 1,
         total: formatNumberWithCommas(currentTotal.toFixed(2)),
         compoundingAmount: formatNumberWithCommas((compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn).toFixed(2)),
+        contributionAmount: formatNumberWithCommas(contribution.toFixed(2)),
       };
     });
 
@@ -49,56 +53,61 @@ const RetirementPlanner = () => {
     setCurrentAssets('');
     setYearsTillRetirement('');
     setEstimatedReturn('');
+    setContributionAmount('');
     setCompoundingFrequency('yearly');
   };
 
   return (
     <div>
-      
       <h1>Savings Planner</h1>
 
       <div className='inputs'>
-      <div>
-        <label>Current Assets:</label>
-        <input type="text" value={`$${currentAssets}`} onChange={(e) => setCurrentAssets(e.target.value.replace('$', ''))} />
-      </div>
-      <div>
-        <label>Compounding Frequency:</label>
-        <select value={compoundingFrequency} onChange={(e) => setCompoundingFrequency(e.target.value)}>
-          <option value="yearly">Yearly</option>
-          <option value="monthly">Monthly</option>
-        </select>
-      </div>
-      <div>
-        <label>Time to save (In Years or Months)</label>
-        <input type="text" value={yearsTillRetirement} onChange={(e) => setYearsTillRetirement(e.target.value)} />
-      </div>
-      <div>
-        <label>Estimated Return (%):</label>
-        <input type="text" value={estimatedReturn} onChange={(e) => setEstimatedReturn(e.target.value)} />
-      </div>
-      
-      <div>
-        <button onClick={handleReset}>Reset</button>
-      </div>
+        <div>
+          <label>Current Assets:</label>
+          <input type="text" value={`$${currentAssets}`} onChange={(e) => setCurrentAssets(e.target.value.replace('$', ''))} />
+        </div>
+        <div>
+          <label>Years to save:</label>
+          <input type="text" value={yearsTillRetirement} onChange={(e) => setYearsTillRetirement(e.target.value)} />
+        </div>
+        <div>
+          <label>Compounding Frequency:</label>
+          <select value={compoundingFrequency} onChange={(e) => setCompoundingFrequency(e.target.value)}>
+            <option value="yearly">Yearly</option>
+            <option value="monthly">Monthly</option>
+          </select>
+        </div>
+        <div>
+          <label>Estimated Return (%):</label>
+          <input type="text" value={estimatedReturn} onChange={(e) => setEstimatedReturn(e.target.value)} />
+        </div>
+        <div>
+          <label>Contribution Amount:</label>
+          <input type="text" value={`$${contributionAmount}`} onChange={(e) => setContributionAmount(e.target.value.replace('$', ''))} />
+        </div>
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
       </div>
 
       <div>
         <h2 className='results-heading'>Results</h2>
         <div className='results-header'>
           <div class="year">Period</div>
-          <div>Return</div>
+          <div>Compounding Amount</div>
+          <div>Contribution Amount</div>
           <div>End Total</div>
         </div>
-          <div class="results">
+        <div class="results">
           {results.map((result) => (
-              <div class="card" key={result.period}>
-                <div class="year">{result.period}</div>
-                <div><span className='dollar-sign'>$</span>{result.compoundingAmount}</div>
-                <div><span className='dollar-sign'>$</span>{result.total}</div>
-              </div>
-            ))}
-          </div>
+            <div class="card" key={result.period}>
+              <div class="period">{result.period}</div>
+              <div><span className='dollar-sign'>$</span>{result.compoundingAmount}</div>
+              <div><span className='dollar-sign'>$</span>{result.contributionAmount}</div>
+              <div><span className='dollar-sign'>$</span>{result.total}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
