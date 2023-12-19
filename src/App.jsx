807e952e8129a0632d9b48b10d47c75e281026ac
designerbrett <brettdesigner@gmail.com>
2023-12-19 11:38:06 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,13 +36,16 @@ const RetirementPlanner = () => {
       const yearlyReturn = currentTotal * returnRate;
       const monthlyReturn = yearlyReturn / 12;
 
+      const startingAmount = currentTotal;
+
       currentTotal = currentTotal + (compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn) + contribution;
 
       return {
         period: index + 1,
-        total: formatNumberWithCommas(currentTotal.toFixed(2)),
+        startingAmount: formatNumberWithCommas(startingAmount.toFixed(2)),
         compoundingAmount: formatNumberWithCommas((compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn).toFixed(2)),
         contributionAmount: formatNumberWithCommas(contribution.toFixed(2)),
+        total: formatNumberWithCommas(currentTotal.toFixed(2)),
       };
     });
 
@@ -71,19 +74,19 @@ const RetirementPlanner = () => {
           <input type="text" value={yearsTillRetirement} onChange={(e) => setYearsTillRetirement(e.target.value)} />
         </div>
         <div>
-          <label>Compounding Frequency:</label>
-          <select value={compoundingFrequency} onChange={(e) => setCompoundingFrequency(e.target.value)}>
-            <option value="yearly">Yearly</option>
-            <option value="monthly">Monthly</option>
-          </select>
-        </div>
-        <div>
           <label>Estimated Return (%):</label>
           <input type="text" value={estimatedReturn} onChange={(e) => setEstimatedReturn(e.target.value)} />
         </div>
         <div>
           <label>Contribution Amount:</label>
           <input type="text" value={`$${contributionAmount}`} onChange={(e) => setContributionAmount(e.target.value.replace('$', ''))} />
+        </div>
+        <div>
+          <label>Compounding Frequency:</label>
+          <select value={compoundingFrequency} onChange={(e) => setCompoundingFrequency(e.target.value)}>
+            <option value="yearly">Yearly</option>
+            <option value="monthly">Monthly</option>
+          </select>
         </div>
         <div>
           <button onClick={handleReset}>Reset</button>
@@ -94,6 +97,7 @@ const RetirementPlanner = () => {
         <h2 className='results-heading'>Results</h2>
         <div className='results-header'>
           <div class="year">Period</div>
+          <div>Starting Amount</div>
           <div>Compounding Amount</div>
           <div>Contribution Amount</div>
           <div>End Total</div>
@@ -102,6 +106,7 @@ const RetirementPlanner = () => {
           {results.map((result) => (
             <div class="card" key={result.period}>
               <div class="period">{result.period}</div>
+              <div><span className='dollar-sign'>$</span>{result.startingAmount}</div>
               <div><span className='dollar-sign'>$</span>{result.compoundingAmount}</div>
               <div><span className='dollar-sign'>$</span>{result.contributionAmount}</div>
               <div><span className='dollar-sign'>$</span>{result.total}</div>
