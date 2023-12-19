9ca10803f238ab134df3d0da755dad51ae4d7bd8
designerbrett <brettdesigner@gmail.com>
2023-12-18 16:15:42 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ const RetirementPlanner = () => {
   const [currentAssets, setCurrentAssets] = useState('');
   const [yearsTillRetirement, setYearsTillRetirement] = useState('');
   const [endRetirementTotal, setEndRetirementTotal] = useState('');
+  const [estimatedReturn, setEstimatedReturn] = useState('');
   const [results, setResults] = useState([]);
 
   const calculateRetirementPlan = () => {
     const yearlyContribution = (endRetirementTotal - currentAssets) / yearsTillRetirement;
     let currentTotal = parseFloat(currentAssets);
+    const returnRate = parseFloat(estimatedReturn) / 100;
 
     const newResults = Array.from({ length: parseInt(yearsTillRetirement) }, (_, index) => {
-      currentTotal += yearlyContribution;
+      const yearlyReturn = currentTotal * returnRate;
+      currentTotal = currentTotal + yearlyContribution + yearlyReturn;
+
       return {
         year: index + 1,
         total: currentTotal.toFixed(2),
         contribution: yearlyContribution.toFixed(2),
+        yearlyReturn: yearlyReturn.toFixed(2),
       };
     });
 
@@ -37,6 +42,10 @@ const RetirementPlanner = () => {
         <label>End Retirement Total:</label>
         <input type="number" value={endRetirementTotal} onChange={(e) => setEndRetirementTotal(e.target.value)} />
       </div>
+      <div>
+        <label>Estimated Percent Return:</label>
+        <input type="number" value={estimatedReturn} onChange={(e) => setEstimatedReturn(e.target.value)} />
+      </div>
       <button onClick={calculateRetirementPlan}>Calculate</button>
       <div>
         <h2>Results</h2>
@@ -46,6 +55,7 @@ const RetirementPlanner = () => {
               <th>Year</th>
               <th>Total</th>
               <th>Contribution</th>
+              <th>Yearly Return</th>
             </tr>
           </thead>
           <tbody>
@@ -54,6 +64,7 @@ const RetirementPlanner = () => {
                 <td>{result.year}</td>
                 <td>${result.total}</td>
                 <td>${result.contribution}</td>
+                <td>${result.yearlyReturn}</td>
               </tr>
             ))}
           </tbody>
