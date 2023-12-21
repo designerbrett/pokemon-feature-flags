b7aa966e6cb16d9fb79c367b69916b693062f74c
designerbrett <brettdesigner@gmail.com>
2023-12-20 22:00:24 -0800

    period totals calculation updates

--- src/RetirementPlanner.jsx
+++ src/RetirementPlanner.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Modal from 'react-modal';
 import OptionsModal from './components/OptionsModal';
-import { Link } from 'react-router-dom';
 import { auth } from './components/firebase';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
@@ -12,6 +10,9 @@ const formatNumberWithCommas = (number) => {
 };
 
 const parseFormattedNumber = (formattedNumber) => {
+  if (typeof formattedNumber === 'number') {
+    return formattedNumber;
+  }
   return parseFloat((formattedNumber || '').replace(/,/g, '')) || 0;
 };
 
@@ -61,7 +62,7 @@ const RetirementPlanner = ({ user }) => {
     localStorage.setItem('compoundingFrequency', compoundingFrequency);
 
     calculateRetirementPlan();
-  }, [currentAssets, yearsTillRetirement, estimatedReturn, contributionAmount, compoundingFrequency]);
+  }, [currentAssets, yearsTillRetirement, estimatedReturn, contributionAmount, compoundingFrequency, visibleOptions]);
 
   useEffect(() => {
     calculateRetirementPlan();
@@ -72,28 +73,57 @@ const RetirementPlanner = ({ user }) => {
     const returnRate = parseFloat(estimatedReturn) / 100;
     const compoundingFactor = compoundingFrequency === 'yearly' ? 1 : 12;
     const contribution = parseFormattedNumber(contributionAmount);
-
-    const newResults = Array.from({ length: parseInt(yearsTillRetirement) * compoundingFactor }, (_, index) => {
+  
+    let newResults = [];
+  
+    for (let index = 0; index < parseInt(yearsTillRetirement) * compoundingFactor; index++) {
       const yearlyReturn = currentTotal * returnRate;
       const monthlyReturn = yearlyReturn / 12;
-
-      const startingAmount = currentTotal;
-
-      currentTotal = currentTotal + (compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn);
-
+  
+      // Calculate the starting amount based on the period
+      const startingAmount = index === 0 ? currentTotal : parseFormattedNumber(newResults[index - 1].total);
+  
+      currentTotal = startingAmount + (compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn);
+  
       const totalWithContribution = currentTotal + contribution;
-
-      return {
+  
+      newResults.push({
         period: index + 1,
         startingAmount: formatNumberWithCommas(startingAmount.toFixed(2)),
         compoundingAmount: formatNumberWithCommas((compoundingFrequency === 'yearly' ? yearlyReturn : monthlyReturn).toFixed(2)),
         contributionAmount: formatNumberWithCommas(contribution.toFixed(2)),
         total: formatNumberWithCommas(totalWithContribution.toFixed(2)),
-      };
-    });
-
+      });
+    }
+  
+    const totalContributions = parseFormattedNumber(
+      newResults.reduce((total, result) => total + parseFormattedNumber(result.contributionAmount), 0)
+    ).toFixed(2);
+  
+    const finalBalance = newResults.length > 0 ? parseFormattedNumber(newResults[newResults.length - 1].total) : 0;
+    const compoundInterestAccrued =
+      newResults.length > 0
+        ? finalBalance - parseFormattedNumber(newResults[0].startingAmount) - parseFormattedNumber(totalContributions)
+        : 0;
+  
+    const returnPercentage =
+      newResults.length > 0
+        ? (
+            (finalBalance - parseFormattedNumber(newResults[0]?.startingAmount)) /
+            parseFormattedNumber(newResults[0]?.startingAmount) *
+            100
+          ).toFixed(2)
+        : 0;
+  
+    // Use the newResults array for rendering
     setResults(newResults);
+    setReturnPercentage(returnPercentage);
   };
+  
+  
+  
+  
+  
 
   const handleReset = () => {
     setCurrentAssets('');
@@ -118,6 +148,8 @@ const RetirementPlanner = ({ user }) => {
     return results.reduce((total, result) => total + parseFloat(result[property].replace(/,/g, '')), 0).toFixed(2);
   };
 
+  const [returnPercentage, setReturnPercentage] = useState(0);
+
   const totalContributions = getTotal('contributionAmount');
   const finalBalance = results.length > 0 ? parseFormattedNumber(results[results.length - 1].total) : 0;
   const compoundInterestAccrued = results.length > 0 ? finalBalance - parseFormattedNumber(results[0].startingAmount) - parseFormattedNumber(totalContributions) : 0;
@@ -129,7 +161,7 @@ const RetirementPlanner = ({ user }) => {
 
   return (
     <div>
-        <div className='input-container'>
+      <div className='input-container'>
         <div>
           <button onClick={toggleInputs}>{showInputs ? 'Hide Inputs' : 'Edit Inputs'}</button>
         </div>
@@ -149,16 +181,16 @@ const RetirementPlanner = ({ user }) => {
             <div>
               <label>Years to save:</label>
               <div className="numeric-input">
-              <input
-                type="text"
-                inputMode="numeric"
-                pattern="[0-9]*"
-                value={yearsTillRetirement}
-                onChange={(e) => setYearsTillRetirement(e.target.value)}
-              />
-              <button onClick={() => setYearsTillRetirement((prev) => (prev ? parseInt(prev) - 1 : prev))}>-</button>
-              <button onClick={() => setYearsTillRetirement((prev) => (prev ? parseInt(prev) + 1 : prev))}>+</button>
-            </div>
+                <input
+                  type="text"
+                  inputMode="numeric"
+                  pattern="[0-9]*"
+                  value={yearsTillRetirement}
+                  onChange={(e) => setYearsTillRetirement(e.target.value)}
+                />
+                <button onClick={() => setYearsTillRetirement((prev) => (prev ? parseInt(prev) - 1 : prev))}>-</button>
+                <button onClick={() => setYearsTillRetirement((prev) => (prev ? parseInt(prev) + 1 : prev))}>+</button>
+              </div>
             </div>
             <div>
               <label>Estimated Return (%):</label>
@@ -209,14 +241,14 @@ const RetirementPlanner = ({ user }) => {
       <div>
         <h2 className='results-heading'>Results</h2>
         <div className='options-dropdown'>
-        <button onClick={handleToggleOptionsModal}>Visibility</button>
-        <OptionsModal
-          isOpen={isOptionsModalOpen}
-          onRequestClose={handleToggleOptionsModal}
-          visibleOptions={visibleOptions}
-          handleOptionToggle={handleOptionToggle}
-        />
-      </div>
+          <button onClick={handleToggleOptionsModal}>Visibility</button>
+          <OptionsModal
+            isOpen={isOptionsModalOpen}
+            onRequestClose={handleToggleOptionsModal}
+            visibleOptions={visibleOptions}
+            handleOptionToggle={handleOptionToggle}
+          />
+        </div>
         <div className='results-header'>
           {visibleOptions.period && <div className="year">Period</div>}
           {visibleOptions.starting && <div>Start</div>}
@@ -240,8 +272,8 @@ const RetirementPlanner = ({ user }) => {
           <p><strong>Final Balance:</strong> ${formatNumberWithCommas(finalBalance)}</p>
           <p><strong>Interest Accrued:</strong> ${formatNumberWithCommas(compoundInterestAccrued.toFixed(2))}</p>
           <p><strong>Total Contributions:</strong> ${formatNumberWithCommas(totalContributions)}</p>
-          <p><strong>Return:</strong> {(((finalBalance - parseFormattedNumber(totalContributions)) - parseFormattedNumber(results[0]?.startingAmount)) / parseFormattedNumber(results[0]?.startingAmount) * 100).toFixed(2)}%</p>
-      </div>
+          <p><strong>Return:</strong> {returnPercentage}%</p>
+        </div>
       </div>
     </div>
   );
